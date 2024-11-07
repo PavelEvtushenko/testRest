31bd59fecfeacb2dbaa9c06db1b5a49bb018a1d7
PavelEvtushenko <1993pav.ak47@mail.ru>
2024-11-07 19:08:12 +0700

    обновление 2

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,14 @@ import (
 	"log"
 	todo "restApi"
 	"restApi/pkg/handler"
+	"restApi/pkg/repository"
+	"restApi/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("ошибка сервера 1 %s", err.Error())
